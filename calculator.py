eb7468d8c837091191c359b6268a7cbe4d1a28e2
ethan hinson <ethan.hinson@gmail.com>
2023-12-20 19:34:42 -0500

    Calculator v0.0.0 created skeleton GUI

--- calculator.py
+++ calculator.py
@@ -2,7 +2,64 @@
 
 import PySimpleGUI as sg
 
-layout = [[]]
+sg.set_options(font = ('Franklin', 20), button_element_size = (6,1.5))
+
+button_size = (6,1.5)
+
+line_1 = [
+    sg.Text("Input:"),
+    sg.Push(),
+    sg.Text("Running Total:")
+    ]
+
+line_2 = [
+    sg.Text("Input_number"),
+    sg.Push(),
+    sg.Text("RT_number")
+    ]
+
+line_3 = [
+    sg.Button("7", key = "-SEVEN-", size = button_size),
+    sg.Button("8", key = "-EIGHT-", size = button_size),
+    sg.Button("9", key = "-NINE-", size = button_size),
+    sg.Button("+", key = "-PLUS-", size = button_size)
+    ]
+
+line_4 = [
+    sg.Button("4", key = "-FOUR-", size = button_size),
+    sg.Button("5", key = "-FIVE-", size = button_size),
+    sg.Button("6", key = "-SIX-", size = button_size),
+    sg.Button("-", key = "-MINUS-", size = button_size)
+    ]
+
+line_5 = [
+    sg.Button("1", key = "-ONE-", size = button_size),
+    sg.Button("2", key = "-TWO-", size = button_size),
+    sg.Button("3", key = "-THREE-", size = button_size),
+    sg.Button("•", key = "-MULTIPLY-", size = button_size)
+    ]
+
+line_6 = [
+    sg.Button("0", key = "-ZERO-", expand_x = True, size = button_size),
+    sg.Button(".", key = "-DECIMAL-", size = button_size),
+    sg.Button("÷", key = "-DIVIDE-", size = button_size)
+    ]
+
+line_7 = [
+    sg.Button("Clear All", key = "-CLEAR-", expand_x = True, size = button_size),
+    sg.Button("Del.", key = "-DELETE-", size = button_size),
+    sg.Button("=", key = "-COMPUTE-", size = button_size)
+    ]
+
+layout = [
+    line_1,
+    line_2,
+    line_3,
+    line_4,
+    line_5,
+    line_6,
+    line_7
+    ]
 
 window = sg.Window("Title", layout)
 
